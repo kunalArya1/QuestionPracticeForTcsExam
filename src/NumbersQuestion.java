c9c7475d75123028987df4980e5eda44c6c7f6be
kunalArya1 <kunalkrraj220@gmail.com>
2024-07-03 22:50:08 +0530

    Number related Question Added

--- src/NumbersQuestion.java
+++ src/NumbersQuestion.java
@@ -12,8 +12,30 @@ public class NumbersQuestion {
 
 
 
-*/
-    public static void main(String[] args) {
 
+
+*/
+
+//1. Java Program to find GCD or HCF of two numbers.
+    public static int findGCD(int dividend ,int divisor ){
+        while (dividend % divisor != 0){
+            int rem = dividend % divisor;
+
+            dividend = divisor;
+
+            divisor = rem;
+        }
+        return divisor;
+
+    }
+
+//2. Java Program to find LCM of numbers.
+    public static int findLCM(int num1, int num2){
+        int lcm = (num1 * num2) / findGCD(num1,num2);
+        return lcm;
+    }
+    public static void main(String[] args) {
+        System.out.println("HCF of number is :- " +findGCD(20,12));
+        System.out.println("LCM of numbers id :- "+ findLCM(12,40));
     }
 }
